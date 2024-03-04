16df326428324517748feb1e9f9c24e49422dee5
a-a <detols33@naver.com>
2024-03-04 23:23:44 +0900

    .

--- Assets/DraggableObject.cs
+++ Assets/DraggableObject.cs
@@ -4,21 +4,36 @@ using UnityEngine;
 
 public class DraggableObject : MonoBehaviour
 {
-
-
+    [HideInInspector]
+    private float _magnification = 1.5f;
+    private Vector3 _originScale = Vector3.zero;
+    private Vector3 _originPosition = Vector3.zero;
     // Start is called before the first frame update
     void Start()
     {
+        _originScale = transform.localScale;
+        _originPosition = transform.position;
     }
 
+    private void OnMouseEnter()
+    {
+        Vector3 value = gameObject.transform.localScale;
+        gameObject.transform.localScale = new Vector3(value.x * _magnification, value.y * _magnification, 1);
+    }
+    private void OnMouseExit()
+    {
+        gameObject.transform.localScale  = _originScale;
+    }
     private void OnMouseDrag()
     {
-        //Vector3 newWorldPosition = new Vector3(_controller.currentMousePosition.x, _controller.currentMousePosition.y, _controller.currentMousePosition.z);
-        
-        //var differ = newWorldPosition - transform.position;
-        ////Debug.Log(string.Format("{0},    {1}", newWorldPosition, differ));
-        //_rigidbody.velocity = differ * 10;
-        //_rigidbody.rotation = Quaternion.Euler(new Vector3(_rigidbody.velocity.z, 0, -_rigidbody.velocity.x));
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 objPos = gameObject.transform.position;
+        gameObject.transform.position = new Vector3(mousePos.x, mousePos.y, objPos.z);
+        Debug.Log(gameObject.transform.position);
+    }
+    private void OnMouseUp()
+    {
+        gameObject.transform.position = _originPosition;
     }
     // Update is called once per frame
     void Update()
--- Assets/TowerCardController.cs
+++ Assets/TowerCardController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -8,14 +9,5 @@ public class TowerCardController : MonoBehaviour
 {
     void Start()
     {
-        // Test Code
-        int count = 5;
-        for(int i = 0; i < count; i++)
-        {
-            GameObject obj = Resources.Load<GameObject>("Prefabs/TowerCard");
-            var realObj = Instantiate(obj, this.transform);
-            realObj.transform.position = new Vector3(realObj.transform.position.x + i * 1, realObj.transform.position.y, realObj.transform.position.z);
-        }
     }
 }
-    
